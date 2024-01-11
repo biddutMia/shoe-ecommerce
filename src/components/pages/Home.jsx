b559245579166794b3dd95beddf314f67a6cc51d
biddut mia <test@gamil.com>
2024-01-11 01:29:12 -0800

    shop product added in home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,24 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import TopHeader from "../shared/main-menu/MainMenu";
 import SubMenu from "../shared/sub-menu/SubMenu";
 import { bannerTextContainer } from "../../style.module.css";
 import Footer from "../shared/Footer/Footer";
+import { Link } from "react-router-dom";
+import { useStoreState } from "easy-peasy";
+import ProductCard from "../shared/card/Card";
 
 const Home = () => {
+  const { shop } = useStoreState((state) => state);
+  const shopProduct = shop.shopItems.slice(0, 6);
+
   return (
     <Box>
       <TopHeader />
@@ -24,8 +38,63 @@ const Home = () => {
             <Container maxWidth="md">
               <Box className={bannerTextContainer}>
                 <Box sx={{ color: "red" }}>25% discount</Box>
-                <Button variant="contained" sx={{ position: "absolute" }}>
+                <Button
+                  variant="contained"
+                  component={Link}
+                  sx={{ position: "absolute" }}
+                  to="/shop"
+                >
                   shop now
+                </Button>
+              </Box>
+            </Container>
+            <Container maxWidth="md" sx={{ marginTop: "40px" }}>
+              <Typography
+                variant="h5"
+                component="span"
+                sx={{ borderBottom: "4px solid red", paddingBottom: "5px" }}
+              >
+                Shop Products
+              </Typography>
+              <Box sx={{ marginTop: "30px" }}>
+                <Grid container spacing={1}>
+                  {shopProduct.length !== 0 &&
+                    shopProduct.map((item, ind) => {
+                      const {
+                        id,
+                        description,
+                        imgURL,
+                        imgAlt,
+                        name,
+                        price,
+                        type,
+                      } = item;
+                      return (
+                        <Grid item md={4} key={id} sm={6} xs={12} xl={3}>
+                          <ProductCard
+                            imgUrl={imgURL}
+                            id={id}
+                            name={name}
+                            description={description}
+                            price={price}
+                            imgAlt={imgAlt}
+                          />
+                        </Grid>
+                      );
+                    })}
+                </Grid>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: "10px 0px",
+                }}
+              >
+                <Button variant={"contained"} component={Link} to="/shop">
+                  {" "}
+                  show more
                 </Button>
               </Box>
             </Container>
--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -5,7 +5,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Box, Button, CardActionArea, CardActions, Stack } from "@mui/material";
 import { useStoreActions, useStoreState } from "easy-peasy";
-import { cardImgAndTextContainer, testImg } from "../../../style.module.css";
+import { cardImgAndTextContainer, cardImg } from "../../../style.module.css";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
@@ -14,37 +14,6 @@ const ProductCard = ({ imgUrl, imgAlt, name, description, price, id }) => {
   const wishList = useStoreState((state) => state);
 
   return (
-    // <Card sx={{ width: "100%", boxShadow: "5px 5px 10px grey" }}>
-    //   <CardActionArea>
-    //     <CardMedia
-    //       sx={{ border: "1px solid gray" }}
-    //       component="img"
-    //       height="140"
-    //       image={imgUrl}
-    //       alt={imgAlt}
-    //     />
-    //     <CardContent>
-    //       <Typography gutterBottom variant="h5" component="div">
-    //         {name}
-    //       </Typography>
-    //       <Typography gutterBottom variant="body1" component="div">
-    //         TK.{price}
-    //       </Typography>
-    //       <Typography variant="body2" color="text.secondary">
-    //         {description}
-    //       </Typography>
-    //     </CardContent>
-    //   </CardActionArea>
-    //   <CardActions>
-    //     <Button
-    //       sx={{ width: "100%" }}
-    //       color="primary"
-    //       onClick={() => actions.cart.addToCart(id)}
-    //     >
-    //       ADD TO CART
-    //     </Button>
-    //   </CardActions>
-    // </Card>
     <Box
       className="card"
       sx={{
@@ -55,17 +24,16 @@ const ProductCard = ({ imgUrl, imgAlt, name, description, price, id }) => {
       }}
     >
       <Box className={cardImgAndTextContainer}>
-        <Box sx={{ width: "100%", overflow: "hidden", aspectRatio: "1/1" }}>
+        <Box sx={{ width: "100%", overflow: "hidden", aspectRatio: "1/0.7" }}>
           {" "}
           <img
-            className={testImg}
+            className={cardImg}
             src={imgUrl}
             alt={imgAlt}
             style={{
               width: "100%",
               height: "100%",
-              // height: "120px",
-              // height: "auto",
+
               borderRadius: "5px",
               objectFit: "cover",
             }}
